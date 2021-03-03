17bc8597a43dc60146b4eb02b87bb14c9198582e
Milena <milykar13@icloud.com>
2021-03-03 09:42:47 +0000

    init implemented

--- pig-game/script.js
+++ pig-game/script.js
@@ -7,20 +7,38 @@ const current0El = document.getElementById("current--0");
 const current1El = document.getElementById("current--1");
 const diceEl = document.querySelector(".dice");
 
+const player0El = document.querySelector(`.player--0`);
+const player1El = document.querySelector(`.player--1`);
+
 const btnRollDice = document.querySelector(".btn--roll");
-const btnNewGame = document.querySelector(".btn--new");
 const btnHold = document.querySelector(".btn--hold");
+const btnNewGame = document.querySelector(".btn--new");
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playingGame = true;
+let scores;
+let currentScore;
+let activePlayer;
+let playingGame;
 
-// Inital state for the score of each player
-score0El.textContent = 0;
-score1El.textContent = 0;
+// Inital state of the game
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playingGame = true;
 
-diceEl.classList.add("hidden");
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -64,7 +82,7 @@ btnHold.addEventListener("click", function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     //2. check if the score is at least 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // finish the game
       playingGame = false;
       diceEl.classList.add("hidden");
@@ -80,3 +98,6 @@ btnHold.addEventListener("click", function () {
     }
   }
 });
+
+// btn new game functionality - Reset everything
+btnNewGame.addEventListener("click", init )
